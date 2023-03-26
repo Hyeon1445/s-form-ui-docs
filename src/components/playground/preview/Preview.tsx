60b4c4f099ffe6bc2d17f10a05d6d4407355f3d5
hyeon1445 <lily1445@gmail.com>
2023-03-27 00:51:33 +0900

    feat: create preview, settings-card component

---
+++ src/components/playground/preview/Preview.tsx
@@ -0,0 +1,7 @@
+import * as S from './Preview.style'
+
+const Preview = () => {
+  return <S.Container>Preview</S.Container>
+}
+
+export default Preview
---
+++ src/components/playground/settings-card/SettingsCard.tsx
@@ -0,0 +1,7 @@
+import * as S from './SettingsCard.style'
+
+const SettingsCard = () => {
+  return <S.Container>Settings Card</S.Container>
+}
+
+export default SettingsCard
