ae6e2c645b907685fe516c0cd49a28e40bae28e8
hyeon1445 <lily1445@gmail.com>
2023-05-13 22:39:45 +0900

    feat: add stack component page

---
+++ src/pages/components/stack/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '@components/common/layout'
+import { PageEnum } from '@components/common/layout/sidebar/Sidebar'
+
+const StackPage = () => {
+  return (
+    <Layout page={PageEnum.Stack}>
+      <div>stack</div>
+    </Layout>
+  )
+}
+
+export default StackPage
