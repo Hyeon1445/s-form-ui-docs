1b9e716b74758ca8fafa96ee42df00a03f536f5b
hyeon1445 <lily1445@gmail.com>
2023-05-13 22:34:28 +0900

    feat: apply Input.Label

--- src/pages/components/input/index.tsx
+++ src/pages/components/input/index.tsx
@@ -2,7 +2,6 @@ import Layout from '@components/common/layout'
 import { PageEnum } from '@components/common/layout/sidebar/Sidebar'
 import { Title } from '../formbox'
 import * as yup from 'yup'
-import styled from '@emotion/styled'
 import { Button, FormBox, Input, VStack } from 's-form-ui'
 
 const InputPage = () => {
@@ -24,33 +23,27 @@ const InputPage = () => {
       >
         <VStack gap="0.5rem">
           <Title>FORM</Title>
-          <InputName>
-            <Highlight>*</Highlight>Name
-          </InputName>
-          <Input name="name" placeholder="이름을 입력해 주세요." />
-          <InputName>
-            <Highlight>*</Highlight>Text
-          </InputName>
-          <Input
-            name="text"
-            placeholder="텍스트를 입력해 주세요."
-            style={{ height: '3rem' }}
-          />
-          <InputName>Disabled</InputName>
-          <Input name="disabled" disabled />
+          <VStack>
+            <Input.Label>Name</Input.Label>
+            <Input name="name" placeholder="이름을 입력해 주세요." />
+          </VStack>
+          <VStack>
+            <Input.Label>Text</Input.Label>
+            <Input
+              name="text"
+              placeholder="텍스트를 입력해 주세요."
+              style={{ height: '3rem' }}
+            />
+          </VStack>
+          <VStack>
+            <Input.Label>Disabled</Input.Label>
+            <Input name="disabled" disabled />
+          </VStack>
           <Button style={{ width: '100%' }}>Submit</Button>
         </VStack>
       </FormBox>
     </Layout>
   )
 }
-
-const InputName = styled.p`
-  font-size: 10px;
-`
-const Highlight = styled.span`
-  color: red;
-  font-weight: bold;
-`
 
 export default InputPage
