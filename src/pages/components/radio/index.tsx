2f19bf5b6574b62df25adcbf594b4233268f04ce
hyeon1445 <lily1445@gmail.com>
2023-05-22 01:39:34 +0900

    feat: add radio example

--- src/pages/components/radio/index.tsx
+++ src/pages/components/radio/index.tsx
@@ -1,10 +1,26 @@
 import Layout from '@components/common/layout'
 import { PageEnum } from '@components/common/layout/sidebar/Sidebar'
+import { Button, FormBox, Radio } from 's-form-ui'
+import { Title } from '../formbox'
 
 const RadioPage = () => {
   return (
     <Layout page={PageEnum.Radio}>
-      <div>radio</div>
+      <FormBox
+        initialValues={{ radio: 'option1' }}
+        style={{
+          width: '20rem',
+          border: '2px solid #f2f2f2',
+          padding: '1rem',
+        }}
+        onSubmit={(values) => console.log('submit', values)}
+      >
+        <Title>FORM</Title>
+        <Radio name="radio">
+          <Radio.Label>radio</Radio.Label>
+        </Radio>
+        <Button style={{ width: '100%' }}>Submit</Button>
+      </FormBox>
     </Layout>
   )
 }
