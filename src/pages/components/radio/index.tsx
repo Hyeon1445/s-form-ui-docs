1a569e381f2a8a6a09f23178751f2d721cfb54fe
hyeon1445 <lily1445@gmail.com>
2023-05-27 17:13:49 +0900

    feat: apply radio component

--- src/pages/components/radio/index.tsx
+++ src/pages/components/radio/index.tsx
@@ -1,13 +1,22 @@
 import Layout from '@components/common/layout'
 import { PageEnum } from '@components/common/layout/sidebar/Sidebar'
-import { Button, FormBox, Radio } from 's-form-ui'
+import { Button, FormBox, Radio, Stack } from 's-form-ui'
 import { Title } from '../formbox'
+import { CSSProperties } from 'react'
 
 const RadioPage = () => {
+  const checkedTabStyle: CSSProperties = {
+    backgroundColor: 'yellow',
+  }
   return (
     <Layout page={PageEnum.Radio}>
       <FormBox
-        initialValues={{ radio: 'option1' }}
+        initialValues={{
+          radio: 'option1',
+          radio2: 'option2',
+          radio3: 'option1',
+          radio4: 'option1',
+        }}
         style={{
           width: '20rem',
           border: '2px solid #f2f2f2',
@@ -16,10 +25,71 @@ const RadioPage = () => {
         onSubmit={(values) => console.log('submit', values)}
       >
         <Title>FORM</Title>
-        <Radio name="radio">
-          <Radio.Label>radio</Radio.Label>
-        </Radio>
-        <Button style={{ width: '100%' }}>Submit</Button>
+        <Stack direction="col" gap="1rem">
+          <Radio name="radio">
+            <Radio.Label>radio</Radio.Label>
+            <Stack gap="0.5rem">
+              <Radio.Button value="option1" />
+              <Radio.Button value="option2" />
+              <Radio.Button value="option3" disabled />
+            </Stack>
+          </Radio>
+          <Radio name="radio2">
+            <Radio.Label>radio2</Radio.Label>
+            <Stack gap="0.5rem">
+              <Radio.Button />
+              <Radio.Button value="option2" disabled />
+              <Radio.Button value="option3" disabled />
+            </Stack>
+          </Radio>
+          <Radio name="radio3">
+            <Radio.Label>radio3</Radio.Label>
+            <Stack direction="col" gap="0.5rem" padding="0.5rem0">
+              <Radio.Option value="option1">
+                <Radio.Button />
+                option1
+              </Radio.Option>
+              <Radio.Option value="option2">
+                <Radio.Button />
+                option2
+              </Radio.Option>
+              <Radio.Option value="option3" disabled>
+                <Radio.Button />
+                option3
+              </Radio.Option>
+              <Radio.Option value="option4" disabled>
+                <Radio.Button />
+                option4
+              </Radio.Option>
+            </Stack>
+          </Radio>
+          <Radio name="radio4">
+            <Radio.Label>radio3</Radio.Label>
+            <Stack direction="col" gap="0.5rem" padding="0.5rem0">
+              <Radio.Option value="option1" checkedStyle={checkedTabStyle}>
+                option1
+              </Radio.Option>
+              <Radio.Option value="option2" checkedStyle={checkedTabStyle}>
+                option2
+              </Radio.Option>
+              <Radio.Option
+                value="option3"
+                disabled
+                checkedStyle={checkedTabStyle}
+              >
+                option3
+              </Radio.Option>
+              <Radio.Option
+                value="option4"
+                disabled
+                checkedStyle={checkedTabStyle}
+              >
+                option4
+              </Radio.Option>
+            </Stack>
+          </Radio>
+          <Button style={{ width: '100%' }}>Submit</Button>
+        </Stack>
       </FormBox>
     </Layout>
   )
