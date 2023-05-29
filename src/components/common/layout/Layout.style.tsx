baef7236f5f1206a45a0e4dc376ccc30a101de9b
hyeon1445 <lily1445@gmail.com>
2023-05-29 18:14:39 +0900

    fix: fix layout padding

--- src/components/common/layout/Layout.style.tsx
+++ src/components/common/layout/Layout.style.tsx
@@ -5,7 +5,7 @@ export const ChildrenWrapper = styled.div`
   height: calc(100vh - 3rem);
   width: calc(100vw - 12rem);
   overflow: auto;
-  padding: 1rem;
+  padding: 2rem 3rem;
   @media screen and (max-width: ${theme.mobileMaxWidth}) {
     width: 100vw;
   }
