39b9b82998b639a274ed5afe5b626078fb17e0f3
hyeon1445 <lily1445@gmail.com>
2023-05-29 16:35:16 +0900

    feat: add FieldArray example

--- src/pages/components/array/index.tsx
+++ src/pages/components/array/index.tsx
@@ -1,22 +1,46 @@
 import Layout from '@components/common/layout'
 import { PageEnum } from '@components/common/layout/sidebar/Sidebar'
 import styled from '@emotion/styled'
-import { Button, Form } from 's-form-ui'
+import { Button, FieldArray, Form, Input } from 's-form-ui'
+import * as yup from 'yup'
 
 const ArrayPage = () => {
   return (
     <Layout page={PageEnum.Array}>
       <Form
-        initialValues={{ value: 'value' }}
+        initialValues={{ inputs: ['', '', ''] }}
         style={{
           width: '20rem',
           border: '2px solid #f2f2f2',
           padding: '1rem',
         }}
+        validationSchema={yup.object().shape({
+          inputs: yup.array().of(yup.string().required('필수!!')),
+        })}
         onSubmit={(values) => console.log('submit', values)}
       >
-        <Title>FORM</Title>
-        <Button style={{ width: '100%' }}>Submit</Button>
+        {({ values }) => (
+          <>
+            <Title>FORM</Title>
+            <FieldArray name="inputs">
+              {values.inputs?.map((input, index) => (
+                <Input name={`inputs.${index}`} key={index} />
+              ))}
+              <FieldArray.PushButton value={'push'} />
+              <FieldArray.SwapButton index1={1} index2={3} />
+              <FieldArray.MoveButton from={2} to={4} />
+              <FieldArray.InsertButton index={1} value={'insert'} />
+              <FieldArray.UnShiftButton value={'unshift'} />
+              <FieldArray.RemoveButton index={0} />
+              <FieldArray.PopButton />
+              <FieldArray.ReplaceButton index={0} value={'replace'} />
+            </FieldArray>
+            <Button style={{ width: '100%' }}>Submit</Button>
+            <Button type="button" style={{ width: '100%' }} disabled>
+              Disabled
+            </Button>
+          </>
+        )}
       </Form>
     </Layout>
   )
