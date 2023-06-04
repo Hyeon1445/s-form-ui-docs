bd30d198bb00c67188d714a1ec4c27441e68fecf
hyeon1445 <lily1445@gmail.com>
2023-06-04 21:26:46 +0900

    chore: change casing of the files

---
+++ src/components/form/Basic.tsx
@@ -0,0 +1,39 @@
+import { Button, Form, Input, Stack } from 's-form-ui'
+import * as yup from 'yup'
+
+const BasicForm = () => {
+  const initialValues = { input: '' }
+  const errorMessage = 'This field is required!'
+  const validationSchema = yup.object({
+    input: yup.string().required(errorMessage),
+  })
+
+  const formStyle = {
+    width: '100%',
+    maxWidth: '30rem',
+    boxShadow: '0px 1px 20px rgba(0, 0, 0, 0.1)',
+    backgroundColor: 'white',
+    borderRadius: '0.5rem',
+    padding: '1rem',
+  }
+
+  return (
+    <Form
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      style={formStyle}
+      onSubmit={(values) => console.log('submit', values)}
+    >
+      <Stack direction="col" margin="0 0 1rem">
+        <Input.Label>required input</Input.Label>
+        <Input name="input" />
+      </Stack>
+      <Stack gap="0.5rem" justifyContent="center" flexWrap="wrap">
+        <Button type="reset">Reset</Button>
+        <Button>Submit</Button>
+      </Stack>
+    </Form>
+  )
+}
+
+export default BasicForm
---
+++ src/components/form/Code.tsx
@@ -0,0 +1,49 @@
+import CodeHighlighter from '@components/common/code-highlighter/code-highlighter'
+
+const BasicFormCode = () => {
+  const pre = `import { Button, Form, Input, Stack } from 's-form-ui'
+import * as yup from 'yup'
+  
+const BasicForm = () => {
+  const initialValues = { input: '' }
+  const errorMessage = 'This field is required!'
+  const validationSchema = yup.object({
+    input: yup.string().required(errorMessage),
+  })
+    
+  const formStyle = {
+    width: '100%',
+    maxWidth: '20rem',
+    boxShadow: '0px 1px 20px rgba(0, 0, 0, 0.1)',
+    backgroundColor: 'white',
+    borderRadius: '0.5rem',
+    padding: '1rem',
+  }
+    
+  return (`
+
+  const code = `    <Form
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      style={formStyle}
+      onSubmit={(values) => console.log('submit', values)}
+    >
+      <Stack direction="col" margin="0 0 1rem">
+        <Input.Label>required input</Input.Label>
+        <Input name="input" />
+      </Stack>
+      <Stack gap="0.5rem" flexWrap="wrap">
+        <Button type="reset">Reset</Button>
+        <Button>Submit</Button>
+      </Stack>
+    </Form>`
+
+  const post = `   )
+}
+  
+export default BasicForm`
+
+  return <CodeHighlighter pre={pre} code={code} post={post} />
+}
+
+export default BasicFormCode
