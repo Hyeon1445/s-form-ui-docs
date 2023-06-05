9c61ac6e832514d23a8d9935ad3765a193232005
hyeon1445 <lily1445@gmail.com>
2023-06-05 10:36:08 +0900

    feat: implement basic button example

--- src/pages/components/button/index.tsx
+++ src/pages/components/button/index.tsx
@@ -1,7 +1,9 @@
 import Layout from '@components/common/layout'
 import { PageEnum } from '@components/common/layout/sidebar/Sidebar'
-import { Button, Form } from 's-form-ui'
+import * as S from '@components/common/style/common.style'
+import { Stack } from 's-form-ui'
 import Head from 'next/head'
+import BasicButtonExample from '@components/button/BasicButtonExample'
 
 const ButtonPage = () => {
   return (
@@ -10,17 +12,17 @@ const ButtonPage = () => {
         <title>Button</title>
       </Head>
       <Layout page={PageEnum.Button}>
-        <Form
-          initialValues={{ value: 'value' }}
-          style={{
-            width: '20rem',
-            border: '2px solid #f2f2f2',
-            padding: '1rem',
-          }}
-          onSubmit={(values) => console.log('submit', values)}
-        >
-          <Button style={{ width: '100%' }}>Submit</Button>
-        </Form>
+        <Stack direction="col" gap="1rem">
+          <S.Title>Button</S.Title>
+          <S.Description>{`There are three types of button types: "submit" type for form submission, "reset" type for form reset, and "button" type for performing other functions.`}</S.Description>
+          <Stack direction="col" gap="1rem" margin="3rem 0 0">
+            <S.Title>Basic Buttons</S.Title>
+            <S.ExampleWrapper>
+              <BasicButtonExample />
+            </S.ExampleWrapper>
+            <div>dd</div>
+          </Stack>
+        </Stack>
       </Layout>
     </>
   )
